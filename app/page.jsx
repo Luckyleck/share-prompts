46c9bbf859fe6d1b863c8c16403204ccc0f93cb6
Alex L <luckyleck29@gmail.com>
2024-10-09 13:50:46 -0400

    Components created

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,23 @@
-const Home = () => {
-  return (
-    <section className="w-full flex-center flex-col">
-        
-    </section>
-  )
-}
+import Feed from '@components/Feed';
 
-export default Home
+const Home = () => {
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Discover and Share
+                <span className="orange_gradient text-center max-md:hidden">
+                    <br />
+                    AI-Powered Prompts
+                </span>
+            </h1>
+            <p className="desc text-center">
+                Promptopia is an open source Ai prompting tool for the modern
+                world to discover, create and share creative prompts
+            </p>
+
+            <Feed />
+        </section>
+    );
+};
+
+export default Home;
