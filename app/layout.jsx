9b70a37d8e27e29d9b32ace87ff72b2aa6c74b8c
Alex L <luckyleck29@gmail.com>
2024-10-09 14:39:11 -0400

    session provider created

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,21 +7,23 @@ export const metadata = {
     description: 'Discover and share AI Prompts'
 }
 
-const RootLayout = ({children}) => {
-  return (
-    <html lang="en">
-        <body>
-            <div className="main">
-                <div className="gradient" />
-            </div>
+const RootLayout = ({ children }) => {
+    return (
+        <html lang="en">
+            <body>
+                <Provider>
+                    <div className="main">
+                        <div className="gradient" />
+                    </div>
 
-            <main className="app">
-                <Nav />
-                {children}
-            </main>
-        </body>
-    </html>
-  )
+                    <main className="app">
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
+            </body>
+        </html>
+    )
 }
 
 export default RootLayout
